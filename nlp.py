062eaa7e97edad7735d593653e92583dd961cea0
Pesinn <pesinn@gmail.com>
2022-01-18 09:47:15 +0100

    spacy ref removed from nlp.py

--- nlp.py
+++ nlp.py
@@ -1,10 +1,9 @@
 from numpy import split
-import spacy
-
+import LanguageProcess.spacy as sp
+import LanguageProcess.nlp_nltk as nltk
+import utils as ut
 from spacy.language import Language
 from spacy_langdetect import LanguageDetector
-
-nlp = spacy.load("en_core_web_sm")
 
 def get_lang_detector(nlp, name):
     return LanguageDetector()
@@ -19,90 +18,26 @@ ignore_array = ["Breaking UK News & World News Headlines",
 
 def analyse_nlp(text):
   text = clean_text(text)
-  doc = nlp(text)
-  tokens = get_tokens(doc)
 
-  nlp_data = {
-    "categorized": tokens[0],
-    "entities": get_named_entities(doc),
-    "all_tokens": tokens[1].strip()
-  }
-  
+  # Get data from Spacy
+  nlp_data = sp.natural_language_process(text)
+
+  # Get named entities from nltk
+  named_ent = nltk.named_entities_nltk(text)
+
+  named_ent["entities"] = ut.combine_dictionaries(
+    nlp_data["entities"],
+    named_ent
+  )
   return nlp_data
 
-"""
-Returns data such as:
-{
-  'ADP':  [
-    {'amid': {'l': 'amid'}}
-  ],
-  'NOUN': [
-    {'liaison': {'l': 'liaison'}},
-    {'office': {'l': 'office'}},
-    {'head': {'l': 'head'}},
-    {'protests': {'l': 'protest'}}
-  ]
-}
-"""
-def get_tokens(doc):
-  all_stopwords = nlp.Defaults.stop_words
-  stopword_filter = {"PUNCT", "ADV", "ADP"}
-  categorized = {}
-  all_tokens = ""
-  for token in doc:
-    if token.text not in all_stopwords and token.pos_ not in stopword_filter:
-      obj = {token.text: {"l": token.lemma_.lower()}}
-      try:
-        # Objects should be unique
-        if(obj not in categorized[token.pos_]):
-          categorized[token.pos_].append(obj)
-      except:
-        categorized[token.pos_] = [obj]    
-      all_tokens += token.lemma_.lower()
-      all_tokens += " "
-  return categorized, all_tokens
 
-
-"""
-Returns data such as
-{
-  'Al Jazeera': 'ORG',
-  'Asia Pacific': 'LOC',
-  'China': 'GPE',
-  'Hong Kong': 'GPE'
-  }
-}
-"""
-def get_named_entities(doc):
-  named_entity = {}
-  for entity in doc.ents:
-    text = entity.text.lower()
-    try:
-      named_entity[text] = entity.label_
-    except:
-      named_entity[text] += entity.label_
-  return named_entity
-
-def convert_dict_to_list(dict):
-  l = []
-  for i in dict:
-    l.append({
-      "entity": i,
-      "count": dict[i]["count"],
-      "type": dict[i]["type"]
-    })
-  return l
-  
 def detect_language(doc):
   lang = doc._.language["language"]
   # MongoDB doesn't handle 'id' as langauge
   if lang == "id":
     return "indonesian"
   return lang
-
-def testing(text):
-  doc = nlp(text)
-  print(doc)
 
 def clean_text(text):
   text = remove_punctuations(text)
@@ -116,7 +51,6 @@ def remove_punctuations(text):
     if(i not in punctuation):
       new_text += i
   return new_text
-
 
 # Remove ending such as '’s'
 # Example: "Tesla’s latest Roadster model"
@@ -134,7 +68,7 @@ def remove_ending(text):
         new_text += text[i]
     i += 1
 
-  return text
+  return new_text
 
 # Getting rid of a text like:
 # "... | Daily News"
@@ -162,6 +96,7 @@ def remove_unrelevant_text(text):
           article_text += f" {i}"
   return article_text.strip()
 
+# Split array by specific character
 def split_array(arr, split_by):
   result_arr = []
   for i in arr:
