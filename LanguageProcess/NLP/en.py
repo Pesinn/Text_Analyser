0272e2443b7da3ec7b5b3a3e22dd13f5d3a5c6de
Pesinn <pesinn@gmail.com>
2022-01-24 21:44:27 +0100

    en clean text discards 'll and 's ending

--- LanguageProcess/NLP/en.py
+++ LanguageProcess/NLP/en.py
@@ -3,19 +3,41 @@ def clean_text(text):
   return text
 
 def remove_punctuations(text):
-  punctuation='!?,.:;"\')’‘(_-–'
-  ending = '\''
+  punctuation='!?,.:;"\')(_-–’‘'
+  ending = '\'’'
   new_text = ""
-  latest = ""
+  latest = "--"
+  c = 0
+  num_to_del = 0
   for i in text:
-    if i not in punctuation:
-      if latest in ending and latest != "":
-        latest = i
-      else:
-        new_text += i
-        latest = i
-    else:
-      # Check for trailing 's
-      new_text += " "
+    if num_to_del > 0:    
       latest = i
+    else:
+      if i not in punctuation:
+        # Check for trailing 's
+        if latest in ending:
+          # Look for sometext's
+          if i == "s":
+            latest = i
+            num_to_del = 1
+          # If i == ll (like we'll)
+          if i == 'l' and find(text, c+1) == 'l':
+            latest = i
+            num_to_del = 2
+        else:
+          new_text += i
+          latest = i
+      else:
+        new_text += " "
+        latest = i
+    c += 1
+    num_to_del -= 1
   return new_text
+
+def find(list, index):
+  obj = ""
+  try:
+    obj = list[index]
+  except:
+    obj = ""
+  return obj
