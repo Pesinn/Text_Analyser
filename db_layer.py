5d38a391f888ac85aafd966fc9d2d2e2bcf5fd98
Pesinn <pesinn@gmail.com>
2022-01-26 22:32:15 +0100

    Input added

--- db_layer.py
+++ db_layer.py
@@ -1,4 +1,5 @@
 import pymongo
+import pprint
 from pymongo import (common, helpers, message)
 
 _myclient = pymongo.MongoClient('localhost', 27017)
@@ -16,3 +17,35 @@ def save_object(object):
 
 def save_objects(objects):
   x = _mycol.insert_many(objects)
+
+def recreate_db():
+  drop_collection()
+  remove_text_index()
+  create_db()
+  create_text_index()
+
+def drop_collection():
+  _mycol.drop()
+
+def create_db():
+  name = "news_data_TEST"
+  db = _myclient[name]
+  list_of_db = _myclient.list_database_names()
+  
+  print("All collections:")
+  for i in list_of_db:
+    print("- ", i)
+
+def remove_text_index():
+  name = "$**_text"
+  _mycol.drop_index(name)
+
+  print(f"List of indexes left after removing {name}")
+  pprint.pprint(_mycol.index_information())
+  
+def create_text_index():
+  response = _mycol.create_index([("$**", pymongo.TEXT)],
+                        language_override="article_language")
+  print(response)
+  print(f"List of indexes after adding creating new")
+  pprint.pprint(_mycol.index_information())
