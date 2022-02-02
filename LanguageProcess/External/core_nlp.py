f3cbe4a89e5b7ef0cce7188ea3dbfe4ab10b6d4f
Pesinn <pesinn@gmail.com>
2022-02-02 16:09:01 +0100

    core_nlp dependency added

---
+++ LanguageProcess/External/core_nlp.py
@@ -0,0 +1,21 @@
+import stanza
+
+nlp = stanza.Pipeline('en')
+
+def named_entities(text):
+  doc = nlp(text)
+  named_entity = {}
+  
+  """
+  i holds the following attributes:
+  - text
+  - type
+  - start_char
+  - end_char
+  """
+  for item in doc.entities:
+    try:
+      named_entity[item.text.lower()] = item.type
+    except:
+      named_entity[item.text.lower()] += item.type
+  return named_entity
