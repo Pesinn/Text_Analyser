daf04e32b0eb19e78caff0b817b925b3eb1740e9
Pesinn <pesinn@gmail.com>
2022-04-01 13:52:41 +0200

    remove duplicate words created in utils

--- utils.py
+++ utils.py
@@ -5,4 +5,9 @@ def combine_dictionaries(dict1, dict2):
   merged = dict()
   merged.update(dict1)
   merged.update(dict2)
+  print(merged)
   return merged
+
+def remove_duplicate_words(str):
+  words = str.split()
+  return " ".join(sorted(set(words), key=words.index))
